21e34f4879a0c2b0db0125cf7c9de23f29971e61
Eric Tien <vanttien93@gmail.com>
2021-12-01 17:59:52 +0700

    upload code

---
+++ components/Layout.jsx
@@ -0,0 +1,28 @@
+// Package
+import Head from 'next/head'
+import { Box } from '@chakra-ui/react'
+
+// Components
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+const Layout = ({ children }) => (
+    <>
+        <Head>
+            <title>Travel Rooms Rapid Api</title>
+        </Head>
+        <Box maxWidth='1280px' m='auto'>
+            <header>
+               <Navbar/>
+            </header>
+            <main>
+                {children}
+            </main>
+            <footer>
+                <Footer/>
+            </footer>
+        </Box>
+    </>
+)
+
+export default Layout
